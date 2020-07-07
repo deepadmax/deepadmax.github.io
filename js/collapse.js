3c548096f160f87d02c85dc0e49eb0c0ee24721b
deepadmax <maximillian.strand@gmail.com>
2020-07-07 20:23:37 +0200

    add collapsible sections

--- js/collapse.js
+++ js/collapse.js
@@ -2,24 +2,29 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-console.log("imported collapse!");
-
 for (i = 0; i < coll.length; i++) {
+  coll[i].style.marginBottom = "0vh";
+  
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     
     var content = this.nextElementSibling;
-    
-    console.log(content);
-    
-    if (content.style.display === "none") {
-      content.style.display = "content";
-      console.log("opened");
-    }
-    else {
+        
+    if (content.style.display === "block") {
       content.style.display = "none";
-      console.log("closed");
+      this.style.marginBottom = "0vh";
+    
+    } else {
+      content.style.display = "block";
+      this.style.marginBottom = "1.6vh";
     }
+    
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+      
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
   });
   console.log("function added to " + coll[i]);
 }
